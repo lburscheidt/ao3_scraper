865c28b4d1845f406c98b130c838db1114680e2f
L Burscheidt <burscheidt@zohomail.eu>
2025-08-09 16:38:11 +0200

    Update pipelines.py

--- ao3_scraper/ao3_scraper/pipelines.py
+++ ao3_scraper/ao3_scraper/pipelines.py
@@ -10,4 +10,13 @@ from itemadapter import ItemAdapter
 
 class Ao3ScraperPipeline:
     def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        # converting item to dict above, since dumps only intakes dict.
+        self.file.write(line)                    # writing content in output file.
         return item
+
+    def open_spider(self, spider):
+        self.file = open('result.json', 'w')
+
+    def close_spider(self, spider):
+        self.file.close()
